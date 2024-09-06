9b304c77fa3550741ec25f37969ea8a21b055217
cubeiplkumar <emailanilkumarm@gmail.com>
2024-09-06 12:35:55 +0530

    feature2.1 changes

--- NewApp.java
+++ NewApp.java
@@ -13,6 +13,31 @@ class Circle
         System.out.println("Circumference of circle: " +circum);
     }
 }
+#feature2.1 changes by vivek
+/ A class with public access specifier
+public class MyClass {
+
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
+#end of feature2.1
 class Main
 {
     public static void main(String args[])
