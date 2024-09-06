588693e2d30f279e294fe70614704dfdba28c43e
cubeiplkumar <emailanilkumarm@gmail.com>
2024-09-06 12:40:05 +0530

    feature2.2 changes by vinay

--- NewApp.java
+++ NewApp.java
@@ -13,6 +13,38 @@ class Circle
         System.out.println("Circumference of circle: " +circum);
     }
 }
+#feature2.2 changes by vinay
+public class RectangleAreaCalculator {
+
+    // User-defined method to calculate the area of a rectangle
+    public static double calculateRectangleArea(double length, double width) {
+        double area = length * width;
+        return area;
+    }
+
+    public static void main(String[] args) {
+        // Example of using the method
+        double length = 5.0;
+        double width = 3.0;
+
+        // Calling the method
+        double result = calculateRectangleArea(length, width);
+
+        // Displaying the result
+        System.out.println("The area of the rectangle with length " + length + " and width " + width + " is: " + result);
+    }
+}
+// Constructor to initialize the instance variables
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Instance method to bark
+    public void bark() {
+        System.out.println(name + " says Woof!");
+    }
+#end of feature2.2
 class Main
 {
     public static void main(String args[])
